91981dc6e541a0a30390648c249c670841aa910b
excitingmvr <excitingmvr@gmail.com>
2022-02-24 09:21:54 +0900

    asdf

---
+++ src/main/java/com/junefw/infra/book/BookController.java
@@ -0,0 +1,40 @@
+package com.junefw.infra.book;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping(value = "/book/")
+public class BookController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "bookList", method = RequestMethod.GET)
+	public String bookList(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "bookList";
+	}
+	
+}
